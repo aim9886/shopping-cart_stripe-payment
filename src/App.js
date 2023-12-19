41144559e369ab2c72df434187ba169562cc1207
aim9886 <aimanth9886@gmail.com>
2023-12-19 21:49:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import Headers from './components/Headers';
+import Home from './components/Home';
+import CartDetails from './components/CartDetails';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes,Route} from "react-router-dom"
+import toast, { Toaster } from 'react-hot-toast';
+import Sucess from './components/Sucess';
+import Cancel from './components/Cancel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Headers />
+     <Routes>
+      <Route  path='/' element={<Home />}/>
+      <Route  path='/cart' element={<CartDetails />}/>
+      <Route  path='/sucess' element={<Sucess />}/>
+      <Route  path='/cancel' element={<Cancel />}/>
+     </Routes>
+     <Toaster />
+    </>
   );
 }
 
